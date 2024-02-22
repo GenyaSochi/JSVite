83bfc6abed267cb8921e74adc221ce10cffc2939
GenyaSochi <eunasedkina@gmail.com>
2024-02-22 21:23:15 +0300

    _

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,31 @@
 import './style.css'
 console.log('hhh')
+console.log(100)
+//dz1
+{
+  let num = -1;
+  let rezult
+  if (num > 0) {
+    rezult = 1
+  } else if (num < 0) {
+    rezult = -1
+  } else {
+    rezult = 0
+  }
+
+  console.log(rezult)
+}
+
+{
+  let num = -1;
+  let rezult
+  if (num > 0) {
+    rezult = 1
+  } else if (num < 0) {
+    rezult = -1
+  } else {
+    rezult = 0
+  }
+
+  console.log(rezult)
+}
